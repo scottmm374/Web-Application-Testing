91dd04caba394de2d6645a33ed280484d61b8da8
Michelle Scott <scottmm374@gmail.com>
2019-10-13 10:39:10 -0400

    Final cleaning up code

--- baseball-stadium/src/App.test.js
+++ baseball-stadium/src/App.test.js
@@ -1,73 +1,67 @@
-import React from 'react';
-import * as rtl from '@testing-library/react';
+import React from "react";
+import * as rtl from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
-import App from './App';
-import { fireEvent } from '@testing-library/react';
-
+import App from "./App";
+import { fireEvent } from "@testing-library/react";
 
 afterEach(rtl.cleanup);
 
-test('ball is found', () => {
-  const {getByText} = rtl.render(<App />);
-  getByText('Ball');
+test("ball is found", () => {
+  const { getByText } = rtl.render(<App />);
+  getByText("Ball");
 });
 
-test('strike is found', () => {
-  const {getByText} = rtl.render(<App />);
-  getByText('Strike');
+test("strike is found", () => {
+  const { getByText } = rtl.render(<App />);
+  getByText("Strike");
 });
 
-test('foul is found', () => {
-  const {getByText} = rtl.render(<App />);
-  getByText('Foul');
+test("foul is found", () => {
+  const { getByText } = rtl.render(<App />);
+  getByText("Foul");
 });
 
-test('hit is found', () => {
-  const {getByText} = rtl.render(<App />);
-  getByText('Hit');
+test("hit is found", () => {
+  const { getByText } = rtl.render(<App />);
+  getByText("Hit");
 });
 
-test(' hit increments by 1', () => {
-  const {getByText} = rtl.render(<App />);
+test(" hit increments by 1", () => {
+  const { getByText } = rtl.render(<App />);
   fireEvent(
-    getByText('Hit'),
-    new MouseEvent('click', {
-      hit: 1,
-      
+    getByText("Hit"),
+    new MouseEvent("click", {
+      hit: 1
     })
-  )
-})
+  );
+});
 
-test(' balls increments by 1', () => {
-  const {getByText} = rtl.render(<App />);
+test(" balls increments by 1", () => {
+  const { getByText } = rtl.render(<App />);
   fireEvent(
-    getByText('Ball'),
-    new MouseEvent('click', {
-      balls: 1,
-      
+    getByText("Ball"),
+    new MouseEvent("click", {
+      balls: 1
     })
-  )
-})
+  );
+});
 
-test(' strikes increments by 1', () => {
-  const {getByText} = rtl.render(<App />);
+test(" strikes increments by 1", () => {
+  const { getByText } = rtl.render(<App />);
   fireEvent(
-    getByText('Strike'),
-    new MouseEvent('click', {
-      strikes: 1,
-      
+    getByText("Strike"),
+    new MouseEvent("click", {
+      strikes: 1
     })
-  )
-})
+  );
+});
 
-test(' fouls increments by 1', () => {
-  const {getByText} = rtl.render(<App />);
+test(" fouls increments by 1", () => {
+  const { getByText } = rtl.render(<App />);
   fireEvent(
-    getByText('Foul'),
-    new MouseEvent('click', {
-      fouls: 1,
-      
+    getByText("Foul"),
+    new MouseEvent("click", {
+      fouls: 1
     })
-  )
-})
-
+  );
+});
